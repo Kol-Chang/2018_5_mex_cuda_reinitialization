c76d988835abd436435b0924ce26020964f0cb15
Tiankui Zhang <tiankuizhang@email.arizona.edu>
2018-02-18 14:43:18 -0700

    changes...

--- src/mexcudaReinitialization/mexcudaReinitialization.hpp
+++ src/mexcudaReinitialization/mexcudaReinitialization.hpp
@@ -3,6 +3,6 @@
 
 void Reinitialization(double * re_lsf, double const * lsf, 
 	int const rows, int const cols, int const pages, 
-	double const dx, double const dy, double const dz)
+	double const dx, double const dy, double const dz);
 
 #endif
