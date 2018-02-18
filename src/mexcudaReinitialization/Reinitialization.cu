06cc16549394b51d4f7fc84784cbb3e5cde65e2a
Tiankui Zhang <tiankuizhang@email.arizona.edu>
2018-02-18 11:39:09 -0700

    changes...

--- src/mexcudaReinitialization/Reinitialization.cu
+++ src/mexcudaReinitialization/Reinitialization.cu
@@ -2,7 +2,19 @@
 #include "mexcudaReinitialization.hpp"
 #include <cuda_runtime_api.h>
 
+__global__ 
+void ExploreIdx()
+{
+	unsigned int const block_idx = blockIdx.x;
+	unsigned int const thread_idx = threadIdx.x;
+} 
 
 void Reinitialization(void){
 	mexPrintf("hello cuda!\n");
+
+	// 
+	dim3 const dimBlock(1,1,1);
+	dim3 const dimThread(2,3,1);
+
+	ExploreIdx<<<dimBlock,dimThread>>>();
 }
