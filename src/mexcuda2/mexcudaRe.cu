38984df987585ace6cf34fa1149d947f793724e2
Tiankui Zhang <tiankuizhang@email.arizona.edu>
2018-02-19 15:53:11 -0700

    changes ...

--- src/mexcuda2/mexcudaRe.cu
+++ src/mexcuda2/mexcudaRe.cu
@@ -32,10 +32,31 @@ void mexFunction(int nlhs , mxArray *plhs[], int nrhs, mxArray const * prhs[])
 		mexPrintf("Get a GPU array \n");
 
 	mxGPUArray const * lsf = mxGPUCreateFromMxArray(prhs[level_set_function]);
+	double const *dev_lsf = (double const *)(mxGPUGetDataReadOnly(lsf)); // pointer to input data on device
+
+	//
+	mxGPUArray const * ds = mxGPUCreateFromMxArray(prhs[grid_spacing]);
+	double const *dev_ds = (double const *)(mxGPUGetDataReadOnly(ds)); 
+
+	/* Create a GPUArray to hold the result and get its underlying pointer. */
+ 	mxGPUArray *re_lsf = mxGPUCreateGPUArray(mxGPUGetNumberOfDimensions(lsf),
+                            				 mxGPUGetDimensions(lsf),
+                            				 mxGPUGetClassID(lsf),
+                            				 mxGPUGetComplexity(lsf),
+                            				 MX_GPU_DO_NOT_INITIALIZE);
+
+ 	double * dev_re_lsf = (double *)(mxGPUGetData(re_lsf)); // pointer to data on device
+
+	/* Wrap the result up as a MATLAB gpuArray for return. */
+    plhs[reinitialized_lsf] = mxGPUCreateMxArrayOnGPU(re_lsf);
 
 
-
+    /*
+     * The mxGPUArray pointers are host-side structures that refer to device
+     * data. These must be destroyed before leaving the MEX function.
+     */
 	mxGPUDestroyGPUArray(lsf);
+	mxGPUDestroyGPUArray(ds);
 }
 
 
