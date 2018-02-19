e7cbcdd541c0db3a84f30856534877e3c95c8350
Tiankui Zhang <tiankuizhang@email.arizona.edu>
2018-02-18 17:07:33 -0700

    changes...

--- src/mexcudaReinitialization/Reinitialization.cu
+++ src/mexcudaReinitialization/Reinitialization.cu
@@ -3,11 +3,19 @@
 #include <cuda_runtime_api.h>
 
 __global__ 
-void ExploreIdx(double * dev_re_lsf, double const * const dev_lsf)
+void ExploreIdx(double * const dev_re_lsf, double const * const dev_lsf, int const number_of_elements_lsf,
+	int const rows, int const cols, int const pages)
 {
-	unsigned int const block_idx = blockIdx.x;
-	unsigned int const thread_idx = threadIdx.x;
-	//mexPrintf("This is block (%d)", blockIdx.x);
+	int row_idx = blockIdx.x * blockDim.x + threadIdx.x;
+	int col_idx = blockIdx.y * blockDim.y + threadIdx.y;
+	int pge_idx = blockIdx.z * blockDim.z + threadIdx.z;
+
+	int idx = pge_idx * rows * cols + col_idx * rows + row_idx;
+
+	if(idx > number_of_elements_lsf)
+		return;
+
+	dev_re_lsf[idx] = dev_lsf[idx];
 } 
 
 /*
@@ -26,11 +34,11 @@ void Reinitialization(double * re_lsf, double const * lsf, int const number_of_e
 
 	// 
 	int dimx = rows, dimy = 4, dimz = 1;
-	dim3 const block(dimx, dimy, dimz);
-	dim3 const grid((rows + block.x - 1) / block.x, (cols + block.y - 1) / block.y, (pages + block.z - 1) / block.z);
+	dim3 const thread(dimx, dimy, dimz);
+	dim3 const block((rows + thread.x - 1) / thread.x, (cols + thread.y - 1) / thread.y, (pages + thread.z - 1) / thread.z);
 
+	mexPrintf("thread dimension (%d,%d,%d)\n", thread.x, thread.y, thread.z);
 	mexPrintf("block dimension (%d,%d,%d)\n", block.x, block.y, block.z);
-	mexPrintf("grid dimension (%d,%d,%d)\n", grid.x, grid.y, grid.z);
 
 	// allocate memory for input lsf and out put level set function
 	double * dev_lsf, * dev_re_lsf;
@@ -43,7 +51,7 @@ void Reinitialization(double * re_lsf, double const * lsf, int const number_of_e
 	cudaMemcpy((void *)dev_lsf, lsf, sizeof(double)*number_of_elements_lsf, cudaMemcpyHostToDevice);
 	//cudaMemset((void *)dev_re_lsf, (int)0, sizeof(double)*number_of_elements_lsf);
 
-	ExploreIdx<<<grid,block>>>(dev_re_lsf, dev_lsf);
+	ExploreIdx<<<grid,block>>>(dev_re_lsf, dev_lsf, number_of_elements_lsf, rows, cols, pages);
 
 	// copy results back 
 	cudaMemcpy(re_lsf, (void *)dev_re_lsf, sizeof(double)*number_of_elements_lsf, cudaMemcpyDeviceToHost);
