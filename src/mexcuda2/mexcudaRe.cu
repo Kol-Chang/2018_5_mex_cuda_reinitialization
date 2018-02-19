77908a833d81225e61d62a7d6a08524174e2d8fb
Tiankui Zhang <tiankuizhang@email.arizona.edu>
2018-02-19 16:03:05 -0700

    changes ...

--- src/mexcuda2/mexcudaRe.cu
+++ src/mexcuda2/mexcudaRe.cu
@@ -47,9 +47,56 @@ void mexFunction(int nlhs , mxArray *plhs[], int nrhs, mxArray const * prhs[])
 
  	double * dev_re_lsf = (double *)(mxGPUGetData(re_lsf)); // pointer to data on device
 
-	/* Wrap the result up as a MATLAB gpuArray for return. */
-    plhs[reinitialized_lsf] = mxGPUCreateMxArrayOnGPU(re_lsf);
 
+    /*
+     * workspace gpuArrays
+     */
+    mxGPUArray *xpr = mxGPUCreateGPUArray(mxGPUGetNumberOfDimensions(lsf),
+                            				 mxGPUGetDimensions(lsf),
+                            				 mxGPUGetClassID(lsf),
+                            				 mxGPUGetComplexity(lsf),
+                            				 MX_GPU_DO_NOT_INITIALIZE);
+ 	double * dev_xpr = (double *)(mxGPUGetData(xpr)); // pointer to data on device
+
+ 	mxGPUArray *ypf = mxGPUCreateGPUArray(mxGPUGetNumberOfDimensions(lsf),
+                            				 mxGPUGetDimensions(lsf),
+                            				 mxGPUGetClassID(lsf),
+                            				 mxGPUGetComplexity(lsf),
+                            				 MX_GPU_DO_NOT_INITIALIZE);
+ 	double * dev_ypf = (double *)(mxGPUGetData(ypf)); // pointer to data on device
+
+ 	mxGPUArray *zpu = mxGPUCreateGPUArray(mxGPUGetNumberOfDimensions(lsf),
+                            				 mxGPUGetDimensions(lsf),
+                            				 mxGPUGetClassID(lsf),
+                            				 mxGPUGetComplexity(lsf),
+                            				 MX_GPU_DO_NOT_INITIALIZE);
+ 	double * dev_zpu = (double *)(mxGPUGetData(zpu)); // pointer to data on device
+
+ 	mxGPUArray *new_lsf = mxGPUCreateGPUArray(mxGPUGetNumberOfDimensions(lsf),
+                            				 mxGPUGetDimensions(lsf),
+                            				 mxGPUGetClassID(lsf),
+                            				 mxGPUGetComplexity(lsf),
+                            				 MX_GPU_DO_NOT_INITIALIZE);
+ 	double * dev_new_lsf = (double *)(mxGPUGetData(new_lsf)); // pointer to data on device
+
+ 	mxGPUArray *intermediate_lsf = mxGPUCreateGPUArray(mxGPUGetNumberOfDimensions(lsf),
+                            				 mxGPUGetDimensions(lsf),
+                            				 mxGPUGetClassID(lsf),
+                            				 mxGPUGetComplexity(lsf),
+                            				 MX_GPU_DO_NOT_INITIALIZE);
+ 	double * dev_intermediate_lsf = (double *)(mxGPUGetData(intermediate_lsf)); // pointer to data on device
+
+ 	mxGPUArray *cur_lsf = mxGPUCreateGPUArray(mxGPUGetNumberOfDimensions(lsf),
+                            				 mxGPUGetDimensions(lsf),
+                            				 mxGPUGetClassID(lsf),
+                            				 mxGPUGetComplexity(lsf),
+                            				 MX_GPU_DO_NOT_INITIALIZE);
+ 	double * dev_cur_lsf = (double *)(mxGPUGetData(cur_lsf)); // pointer to data on device
+
+
+
+ 	/* Wrap the result up as a MATLAB gpuArray for return. */
+    plhs[reinitialized_lsf] = mxGPUCreateMxArrayOnGPU(re_lsf);
 
     /*
      * The mxGPUArray pointers are host-side structures that refer to device
@@ -57,6 +104,12 @@ void mexFunction(int nlhs , mxArray *plhs[], int nrhs, mxArray const * prhs[])
      */
 	mxGPUDestroyGPUArray(lsf);
 	mxGPUDestroyGPUArray(ds);
+	mxGPUDestroyGPUArray(xpr);
+	mxGPUDestroyGPUArray(ypf);
+	mxGPUDestroyGPUArray(zpu);
+	mxGPUDestroyGPUArray(new_lsf);
+	mxGPUDestroyGPUArray(intermediate_lsf);
+	mxGPUDestroyGPUArray(cur_lsf);
 }
 
 
