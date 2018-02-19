38b72ee41be61d63356cb042db254f16c5df54fc
Tiankui Zhang <tiankuizhang@email.arizona.edu>
2018-02-19 16:33:11 -0700

    changes ...

--- src/mexcuda2/Re.cu
+++ src/mexcuda2/Re.cu
@@ -9,8 +9,7 @@ void Reinitialization(double * const dev_re_lsf, double const * const dev_lsf,
 	double * const dev_xpr, double * const dev_ypf, double * const dev_zpu, 
 	double * dev_new_lsf, double * dev_intermediate_lsf, double * dev_cur_lsf,
 	int const number_of_elements_lsf, int const rows, int const cols, int const pages,
-	double const dx, double const dy, double const dz);
-
+	double const dx, double const dy, double const dz)
 {
 	mexPrintf("try again ...\n");
 }
