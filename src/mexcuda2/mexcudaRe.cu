141d75fa5c449cefff617e9ce4ac03a2a1b04a4d
Tiankui Zhang <tiankuizhang@email.arizona.edu>
2018-02-26 15:45:18 -0700

    update github ...

--- src/mexcuda2/mexcudaRe.cu
+++ src/mexcuda2/mexcudaRe.cu
@@ -136,7 +136,10 @@ void mexFunction(int nlhs , mxArray *plhs[], int nrhs, mxArray const * prhs[])
 
 	/* Wrap the result up as a MATLAB cpuArray for return. */
 	mexPrintf("trying to return a cpu array \n");
-    plhs[reinitialized_lsf] = mxGPUCreateMxArrayOnCPU(cur_lsf);
+    //plhs[reinitialized_lsf] = mxGPUCreateMxArrayOnCPU(cur_lsf);
+    plhs[reinitialized_lsf] = mxGPUCreateMxArrayOnGPU(cur_lsf);
+
+    mexPrintf("returned a cpu array \n");
 
     /*
      * The mxGPUArray pointers are host-side structures that refer to device
