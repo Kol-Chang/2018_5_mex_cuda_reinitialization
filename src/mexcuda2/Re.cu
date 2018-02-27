fb762a2a4f908d211ff95c8e5c762cde5f58a17b
Tiankui Zhang <tiankuizhang@email.arizona.edu>
2018-02-26 17:59:42 -0700

    update github ...

--- src/mexcuda2/Re.cu
+++ src/mexcuda2/Re.cu
@@ -339,6 +339,8 @@ void Reinitialization(double * dev_re_lsf, double const * const dev_lsf,
 
 	explore<<<block,thread>>>(dev_re_lsf,dev_lsf,number_of_elements_lsf,rows,cols,pages);
 
+	mxInitGPU();
+
 	boundary_correction<<<block, thread>>>(dev_xpr, dev_ypf, dev_zpu, 
 		dev_lsf, dev_re_lsf,
 		number_of_elements_lsf, rows, cols, pages, dx, dy, dz);
