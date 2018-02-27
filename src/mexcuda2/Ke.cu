315aa9bb5230c089aaec3aa4800e0946081dbb84
Tiankui Zhang <tiankuizhang@email.arizona.edu>
2018-02-26 17:08:49 -0700

    update github ...

--- src/mexcuda2/Ke.cu
+++ src/mexcuda2/Ke.cu
@@ -1,9 +1,5 @@
 #include <algorithm>
 #include <cuda_runtime_api.h>
-#include "mex.h"
-#include "gpu/mxGPUArray.h"
-
-#include "Re.hpp"
 
 /**	
  * convert subindices to global indices 
@@ -319,65 +315,3 @@ void copy_array(double * const dev_re_lsf, double const * const dev_lsf,
 	dev_re_lsf[idx] = dev_lsf[idx];
 }
 
-void Reinitialization(double * dev_re_lsf, double const * const dev_lsf, 
-	double * const dev_xpr, double * const dev_ypf, double * const dev_zpu, 
-	double * dev_new_lsf, double * dev_intermediate_lsf, double * dev_cur_lsf,
-	int const number_of_elements_lsf, int const rows, int const cols, int const pages,
-	double const dx, double const dy, double const dz)
-{
-	mexPrintf("reintializing ...\n");
-
-	int dimx = rows, dimy = 4, dimz = 1;
-	dim3 const thread(dimx, dimy, dimz);
-	dim3 const block(	(rows + thread.x - 1)  / thread.x, 
-						(cols + thread.y - 1)  / thread.y, 
-						(pages + thread.z - 1) / thread.z);
-
-	
-	// fill in dev_xpr,ypf,zpu
-	
-	//boundary_correction<<<block, thread>>>(dev_xpr, dev_ypf, dev_zpu, 
-	//	dev_lsf, dev_cur_lsf,
-	//	number_of_elements_lsf, rows, cols, pages, dx, dy, dz);
-
-	boundary_correction<<<block, thread>>>(dev_xpr, dev_ypf, dev_zpu, 
-		dev_lsf, dev_re_lsf,
-		number_of_elements_lsf, rows, cols, pages, dx, dy, dz);
-
-	// iteration
-	for(int i = 0;i < 100; ++i){
-
-		mexPrintf("%d\n",i);
-
-		//cudaDeviceSynchronize();
-		//time_step_lsf<<<block, thread>>>(dev_new_lsf, dev_intermediate_lsf, dev_cur_lsf, dev_lsf, 
-		//	dev_xpr, dev_ypf, dev_zpu, 
-		//	number_of_elements_lsf, rows, cols, pages, dx, dy, dz, true); 	
-
-		time_step_lsf<<<block, thread>>>(dev_new_lsf, dev_intermediate_lsf, dev_re_lsf, dev_lsf, 
-			dev_xpr, dev_ypf, dev_zpu, 
-			number_of_elements_lsf, rows, cols, pages, dx, dy, dz, true); 	
-
-		//cudaDeviceSynchronize();
-		//time_step_lsf<<<block, thread>>>(dev_new_lsf, dev_cur_lsf, dev_intermediate_lsf, dev_lsf, 
-		//	dev_xpr, dev_ypf, dev_zpu, 
-		//	number_of_elements_lsf, rows, cols, pages, dx, dy, dz, false);
-
-		time_step_lsf<<<block, thread>>>(dev_new_lsf, dev_re_lsf, dev_intermediate_lsf, dev_lsf, 
-			dev_xpr, dev_ypf, dev_zpu, 
-			number_of_elements_lsf, rows, cols, pages, dx, dy, dz, false); 
-
-		//cudaDeviceSynchronize();
-		//std::swap(dev_new_lsf,dev_cur_lsf);
-		std::swap(dev_new_lsf,dev_re_lsf);
-
-		//copy_array<<<block,thread>>>(dev_cur_lsf,dev_new_lsf,number_of_elements_lsf,rows,cols,pages);
-
-	}
-
-	mexPrintf("finished!\n");
-
-	//std::swap(dev_re_lsf,dev_xpr);
-
-	//copy_array<<<block, thread>>>(dev_re_lsf, dev_cur_lsf, number_of_elements_lsf, rows, cols, pages);
-}
